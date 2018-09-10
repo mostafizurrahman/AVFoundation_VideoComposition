a63cc5a7a93128459a0617b9f7579351eec59512
Mostafizur Rahman <mostafizur.cse@gmail.com>
2018-09-10 16:27:33 +0600

    video effect done

--- RWVideoNinja/VideoFilterCompositor.swift
+++ RWVideoNinja/VideoFilterCompositor.swift
@@ -42,10 +42,15 @@ class VideoFilterCompositor : NSObject, AVVideoCompositing{
   
   let renderQueue = DispatchQueue(label: "com.jojodmo.videofilterexporter.renderingqueue", attributes: [])
   let renderContextQueue = DispatchQueue(label: "com.jojodmo.videofilterexporter.rendercontextqueue", attributes: [])
-  
+  let transition = CIFilter.init(name: "CIDissolveTransition")
   var renderContext: AVVideoCompositionRenderContext!
   override init(){
     super.init()
+    
+//    self.transition?.setValue(CGPoint.init(x: 200, y: 500), forKey: "inputCenter")
+//    self.transition?.setValue(CGRect.init(x: 0, y: 0, width: 720, height: 1280), forKey: "inputExtent")
+    self.transition?.setValue(0.100, forKey: "inputTime")
+    
   }
   
   func startRequest(_ request: AVAsynchronousVideoCompositionRequest) {
@@ -66,19 +71,24 @@ class VideoFilterCompositor : NSObject, AVVideoCompositing{
         }
         
         var image = CIImage(cvPixelBuffer: pixels)
-        var image2 = CIImage(cvPixelBuffer: pixels2).app
-        for filter in instruction.filters{
-          filter.setValue(image, forKey: kCIInputImageKey)
-          image = filter.outputImage ?? image
-          filter.setValue(image2, forKey: kCIInputImageKey)
-          image2 = filter.outputImage ?? image2
-        }
+        let image2 = CIImage(cvPixelBuffer: pixels2)
+        
+        transition?.setValue(image, forKey: "inputTargetImage")
+        transition?.setValue(image2, forKey: "inputImage")
+        let fimage = transition?.outputImage
+        image = fimage ?? image
+//        for filter in instruction.filters{
+//          filter.setValue(image, forKey: kCIInputImageKey)
+//          image = filter.outputImage ?? image
+//          filter.setValue(image2, forKey: kCIInputImageKey)
+//          image2 = filter.outputImage ?? image2
+//        }
         
         let newBuffer: CVPixelBuffer? = self.renderContext.newPixelBuffer()
         
         if let buffer = newBuffer {
           instruction.context.render(image, to: buffer)
-          instruction.context.render(image2, to: buffer)
+//          instruction.context.render(image2, to: buffer)
           request.finish(withComposedVideoFrame: buffer)
         }
         else{
